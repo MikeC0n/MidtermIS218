70318767a30e25fb3539093d42312eabe7c95f3a
Michael <mjc26@njit.edu>
2024-11-06 20:38:12 -0500

    Added SOLID Principle to map out all arthmetic functions and added Factory method for calculator operations.

--- app/operations.py
+++ app/operations.py
@@ -79,3 +79,76 @@ class Division(Operation):
         """Division of a by b."""
         self.validate_operands(a, b)
         return a / b
+
+class Power(Operation):
+    """Exponential Function"""
+
+    def validate_operands(sellf, a: Decimal, b: Decimal) -> None:
+        """Validate input for accepted exponents"""
+        super().validate_operands(a, b)
+        if b < 0:
+            raise ValidationError("Negative exponents are not allowed")
+
+    def execute(self, a: Decimal, b: Decimal) -> Decimal:
+        """Calculate a number, a, raised to the power b."""
+        self.validate_operands(a, b)
+        return Decimal(pow(float(a), float(b))) # Type casting
+
+class Root(Operation):
+    """Root Function"""
+
+    def validate_operands(sellf, a: Decimal, b: Decimal) -> None:
+        """Validate inputs for root operation"""
+        super().validate_operands(a, b)
+        if a < 0:
+            raise ValidationError("Root of negative number cannot be calculated")
+        if b == 0:
+            raise ValidationError("Zero root is undefined")
+
+    def execute(self, a: Decimal, b: Decimal) -> Decimal:
+        self.validate_operands(a, b)
+        return Decimal(pow(float(a), 1/float(b)))
+
+# Factory Method
+class OperationFactory:
+    """Factory class for creating operation instances"""
+    _operations: Dict[str, type] = { # Dictionary Implementation
+        'add': Addition,
+        'subtract': Subtraction,
+        'multiply': Multiplication,
+        'divide': Division,
+        'power': Power,
+        'root': Root
+    }
+
+    @classmethod
+    def register_operation(cls, name: str, operation_class: type) -> None:
+        """
+        Cycle new operation type
+
+        Arguments:
+            name: Operation Name
+            operation_class: Operation name pair in dictionary
+        """
+        if not issubclass(operation_class, Operation):
+            raise TypeError("Operation class must inherit from Operation") # LBYL
+        cls._operations[name.lower()] = operation_class
+
+    @classmethod
+    def create_operation(cls, operation_type: str) -> Operation:
+        """
+        Create operation instance
+
+        Arguments:
+            operation_type: type of operation to create
+
+        Returns:
+            Operation Instance
+
+        Raises:
+            ValueError: If the operation does not exist
+        """
+        operation_class = cls.operations.get(operation_type.lower())
+        if not operation_class:
+            raise ValueError(f"Unknown operation: {operation_type}")
+        return operation_class()
